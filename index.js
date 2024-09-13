13d483a83c6a0917880b801b2dd81c78886e907d
aaaathing <aarontao950@gmail.com>
2024-09-12 18:23:48 -0700

    add useful functions

--- index.js
+++ index.js
@@ -4,8 +4,65 @@ window.mcProtocol = {}
 process.versions = {node:"Infinity.Infinity.Infinity"}
 window.mcProtocol.ping = require("minecraft-protocol/src/ping.js")
 window.mcProtocol.conv = require("mineflayer/lib/conversions.js")
+window.mcProtocol.parseMetadata = function(metadata, entityMetadata = {}) {
+  if (metadata !== undefined) {
+    for (const { key, value } of metadata) {
+      entityMetadata[key] = value
+    }
+  }
+  return entityMetadata
+}
+window.mcProtocol.updateAttributes = (packet, attributes) => {
+	if (!attributes) attributes = {}
+	for (const prop of packet.properties) {
+		attributes[prop.key] = {
+			value: prop.value,
+			modifiers: prop.modifiers
+		}
+	}
+}
 
-//from minecraft-protocol/src/createClient.js
+//!from https://github.com/PrismarineJS/prismarine-chunk/blob/master/src/pc/1.8/chunk.js
+const sectionCount = 16
+function parseBitMap (bitMap) {
+  const chunkIncluded = new Array(sectionCount)
+  let chunkCount = 0
+  for (let y = 0; y < sectionCount; ++y) {
+    chunkIncluded[y] = bitMap & (1 << y)
+    if (chunkIncluded[y]) chunkCount++
+  }
+  return { chunkIncluded, chunkCount }
+}
+window.mcProtocol.load_1_8 = function (data, bitMap = 0xFFFF, skyLightSent = true, fullChunk = true, cb) {
+	const { chunkIncluded, chunkCount } = parseBitMap(bitMap)
+	let offset = 0
+	let offsetLight = 16 * 16 * sectionCount * chunkCount * 2
+	let offsetSkyLight = (this.skyLightSent) ? 16 * 16 * sectionCount * chunkCount / 2 * 5 : 0
+	for (let i = 0; i < sectionCount; i++) {
+		if (chunkIncluded[i]) {
+			cb(i, data.subarray(offset, offset + w * l * sh * 2), data.subarray(offsetLight, offsetLight + w * l * sh / 2), this.skyLightSent && data.subarray(offsetSkyLight, offsetSkyLight + w * l * sh / 2))
+			offset += w * l * sh * 2
+			offsetLight += w * l * sh / 2
+			if (this.skyLightSent) offsetSkyLight += w * l * sh / 2
+		}
+	}
+	if (fullChunk) {
+		data.copy(this.biome, 0, w * l * sectionCount * chunkCount * (skyLightSent ? 3 : 5 / 2))
+	}
+
+	const expectedSize = SECTION_SIZE * chunkCount + (fullChunk ? w * l : 0)
+	if (data.length !== expectedSize) { throw (new Error(`Data buffer not correct size (was ${data.length}, expected ${expectedSize})`)) }
+}
+window.mcProtocol.onesInShort = function (n) {
+  n = n & 0xffff
+  let count = 0
+  for (let i = 0; i < 16; ++i) {
+    count = ((1 << i) & n) ? count + 1 : count
+  }
+  return count
+}
+
+//!from minecraft-protocol/src/createClient.js
 let DefaultClientImpl = require("minecraft-protocol/src/client.js")
 const assert = require('assert')
 
